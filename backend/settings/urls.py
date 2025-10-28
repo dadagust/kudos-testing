3466ff486e521c7d18bc8f716f9860c5b71e0560
dadagust <gusitelchiller@gmail.com>
2025-10-28 20:57:39 +0300

    fix

--- backend/settings/urls.py
+++ backend/settings/urls.py
@@ -2,6 +2,8 @@
 
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -10,3 +12,6 @@ urlpatterns = [
     path('api/v1/', include('applications.orders.urls')),
     path('api/v1/', include('applications.products.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
